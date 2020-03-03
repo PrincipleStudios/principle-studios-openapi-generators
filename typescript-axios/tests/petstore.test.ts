ef2671a61bb4d66798d338b39d038349ddb34e07
Matt DeKrey <mattdekrey@gmail.com>
2020-03-02 18:49:50 -0600

    Further improvements to templates

---
+++ typescript-axios/tests/petstore.test.ts
@@ -0,0 +1,15 @@
+import * as path from 'path';
+import { runCodegen } from './runCodegen';
+import { verifyAllFiles } from './verifyAllFiles';
+
+describe('typescript-axios petstore.yaml', () => {
+    const templateFolder = path.join(__dirname, "../templates");
+    const outputDir = path.join(__dirname, "../out.local");
+    const source = path.join(__dirname, "../../samples/petstore.yaml");
+
+    beforeAll(async () => {
+        await runCodegen(outputDir, `generate -i "${source}" -g typescript-axios -t "${templateFolder}"`);
+    })
+
+    verifyAllFiles(outputDir);
+});
