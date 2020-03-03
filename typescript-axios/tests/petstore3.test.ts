7e2db7570e7677c8208a83780c5aad5b62d67c82
Matt DeKrey <mattdekrey@gmail.com>
2020-03-02 19:00:06 -0600

    Add petstore3 file

--- typescript-axios/tests/petstore3.test.ts
+++ typescript-axios/tests/petstore3.test.ts
@@ -4,8 +4,8 @@ import { verifyAllFiles } from './verifyAllFiles';
 
 describe('typescript-axios petstore.yaml', () => {
     const templateFolder = path.join(__dirname, "../templates");
-    const outputDir = path.join(__dirname, "../out.local.petstore");
-    const source = path.join(__dirname, "../../samples/petstore.yaml");
+    const outputDir = path.join(__dirname, "../out.local.petstore3");
+    const source = path.join(__dirname, "../../samples/petstore3.json");
 
     beforeAll(async () => {
         await runCodegen(outputDir, `generate -i "${source}" -g typescript-axios -t "${templateFolder}"`);
