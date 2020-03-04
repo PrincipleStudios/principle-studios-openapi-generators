e4ab3e2fe95067ed627510b7179b0501a4e9cdb0
Matt DeKrey <mattdekrey@gmail.com>
2020-03-04 11:03:26 -0600

    Improve build process

--- dotnetcore-server-interfaces/npm/tests/runCodegen.ts
+++ dotnetcore-server-interfaces/npm/tests/runCodegen.ts
@@ -10,12 +10,10 @@ export function runCodegen(outDir: string, args: string) {
                 reject(err);
             }
 
-            const myJar = path.join(__dirname, "../../java/build/libs/aspnetcore-server-interfaces-1.0.0.jar");
-            const origJar = require.resolve('@openapitools/openapi-generator-cli/bin/openapi-generator.jar');
-            if (!fs.existsSync(myJar)) throw new Error("My jar doesn't exist!");
-            if (!fs.existsSync(origJar)) throw new Error("The original jar doesn't exist!");
+            const myJars = path.join(__dirname, "../../java/build/libs");
+            if (!fs.existsSync(myJars)) throw new Error("My jar doesn't exist!");
 
-            exec(`java -cp "${myJar};${origJar}" org.openapitools.codegen.OpenAPIGenerator ${args}  -g com.principlestudios.codegen.DotNetCoreInterfacesGenerator -o ${outDir}`,
+            exec(`java -cp "${myJars}/*" org.openapitools.codegen.OpenAPIGenerator ${args}  -g com.principlestudios.codegen.DotNetCoreInterfacesGenerator -o ${outDir}`,
                 (error, stdout, stderr) => {
                     if (error !== null) {
                         reject({ stdout, stderr, error });
