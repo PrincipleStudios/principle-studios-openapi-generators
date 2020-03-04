b063639a9207bd525e418f852e07e85d27db9809
Matt DeKrey <mattdekrey@gmail.com>
2020-03-04 08:27:46 -0600

    Move java files

--- dotnetcore-server-interfaces/npm/tests/runCodegen.ts
+++ dotnetcore-server-interfaces/npm/tests/runCodegen.ts
@@ -1,5 +1,7 @@
 import rimraf from 'rimraf';
 import {exec} from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export function runCodegen(outDir: string, args: string) {
     return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
@@ -7,7 +9,13 @@ export function runCodegen(outDir: string, args: string) {
             if (err) {
                 reject(err);
             }
-            exec(`java -cp "../build/libs/aspnetcore-server-interfaces-1.0.0.jar;node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator.jar" org.openapitools.codegen.OpenAPIGenerator ${args}  -g com.principlestudios.codegen.DotNetCoreInterfacesGenerator -o ${outDir}`,
+
+            const myJar = path.join(__dirname, "../../java/build/libs/aspnetcore-server-interfaces-1.0.0.jar");
+            const origJar = require.resolve('@openapitools/openapi-generator-cli/bin/openapi-generator.jar');
+            if (!fs.existsSync(myJar)) throw new Error("My jar doesn't exist!");
+            if (!fs.existsSync(origJar)) throw new Error("The original jar doesn't exist!");
+
+            exec(`java -cp "${myJar};${origJar}" org.openapitools.codegen.OpenAPIGenerator ${args}  -g com.principlestudios.codegen.DotNetCoreInterfacesGenerator -o ${outDir}`,
                 (error, stdout, stderr) => {
                     if (error !== null) {
                         reject({ stdout, stderr, error });
