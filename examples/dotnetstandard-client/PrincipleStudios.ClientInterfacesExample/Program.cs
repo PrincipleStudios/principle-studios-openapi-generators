d828eb80d1cd8c1ffbe2e2ba5a3a3321817b8de4
Matt DeKrey <mattdekrey@gmail.com>
2020-04-16 20:37:17 -0500

    Improve client example

--- examples/dotnetstandard-client/PrincipleStudios.ClientInterfacesExample/Program.cs
+++ examples/dotnetstandard-client/PrincipleStudios.ClientInterfacesExample/Program.cs
@@ -11,23 +11,31 @@ namespace PrincipleStudios.ClientInterfacesExample
         public static async Task Main(string[] args)
         {
             await Petstore();
-            await Petstore3();
         }
 
         private static async Task Petstore()
         {
-            var client = new Clients.Petstore.DefaultApiClient(null, null);
-            var response = await client.AddPetAsync(null);
-            var result = await response.StatusCode200Async();
+            using var httpClient = new HttpClient();
+            var client = new Clients.Petstore.DefaultApiClient(httpClient, new Clients.Petstore.DefaultApiClientConfiguration
+            {
+                BaseUrl = "https://localhost:5001/api",
+                Settings = new Newtonsoft.Json.JsonSerializerSettings()
+            });
+            var createdPetResponse = await client.AddPetAsync(new Clients.Petstore.NewPet(Name: "Fido", Tag: "Dog"));
+            var pet = await createdPetResponse.StatusCode200Async();
+            using var foundPetResponse = await client.FindPetByIdAsync(pet.Id);
+            var foundPet = await foundPetResponse.StatusCode200Async();
+            System.Diagnostics.Debug.Assert(pet.Name == foundPet.Name);
+            using var foundPetsResponse = await client.FindPetsAsync(new[] { "Dog" }, 1);
+            var pets = await foundPetsResponse.StatusCode200Async();
+            System.Diagnostics.Debug.Assert(pets.Count == 1);
+            System.Diagnostics.Debug.Assert(pet.Name == pets[0].Name);
+            using var deletePetResponse = await client.DeletePetAsync(pet.Id);
+            await deletePetResponse.StatusCode204Async();
+            using var finalFindResponse = await client.FindPetsAsync(new[] { "Dog " }, 1);
+            var finalPets = await finalFindResponse.StatusCode200Async();
+            System.Diagnostics.Debug.Assert(finalPets.Count == 0);
         }
 
-        private static async Task Petstore3()
-        {
-            var petClient = new Clients.Petstore3.PetApiClient(null, null);
-            var response = await petClient.DeletePetAsync(null, null);
-
-            var userClient = new Clients.Petstore3.UserApiClient(null, null);
-            await userClient.LoginUserAsync(null, null);
-        }
     }
 }
