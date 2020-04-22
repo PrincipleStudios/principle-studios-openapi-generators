8d8100d89e0ac480f4c61fa9217531c33e361383
Matt DeKrey <mattdekrey@gmail.com>
2020-04-22 07:25:22 -0500

    Allow self-hosted API paths and bump version

--- generators/publish-npm.ps1
+++ generators/publish-npm.ps1
@@ -1,2 +1,2 @@
 npm publish .\out\principlestudios-openapi-codegen-typescript-axios-0.0.2.tgz --access public
-npm publish .\out\principlestudios-openapi-codegen-typescript-rxjs-0.0.2.tgz --access public
+npm publish .\out\principlestudios-openapi-codegen-typescript-rxjs-0.0.3.tgz --access public
