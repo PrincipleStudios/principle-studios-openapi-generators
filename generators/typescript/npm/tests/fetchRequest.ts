804deb0169a3943832b47223644b931954296387
Matt DeKrey <mattdekrey@gmail.com>
2021-09-16 09:45:28 -0500

    Include alternate typing for future TS versions

--- generators/typescript/npm/tests/fetchRequest.ts
+++ generators/typescript/npm/tests/fetchRequest.ts
@@ -17,9 +17,6 @@ function fetchRequest<TParams extends {}, TBody extends RequestBodies, TResponse
     return transform as TransformRequest<TParams, TBody, TCallType, Promise<TResponse>>;
 }
 const fetchLookupRecord = fetchRequest(lookupRecord);
-async function testPipe() {
-    const response: LookupResponses = await fetchLookupRecord({}, { formattedAddress: '123 Main St', location: { latitude: 0, longitude: 0 } }, 'application/json');
-}
 
 type Converted<TConversion extends RequestConversion<any, any, any, any>> = TConversion extends RequestConversion<infer TParams, infer TBody, infer TResponse, infer TCallType> ? TransformRequest<TParams, TBody, TCallType, Promise<TResponse>> : never;
 
@@ -34,3 +31,42 @@ function applyTransform<TMethods extends RequestConversions>(
     });
 }
 const fetchTemp = applyTransform(allOperations, fetchRequest);
+
+async function testPipe() {
+    const response: LookupResponses = await fetchLookupRecord({}, { formattedAddress: '123 Main St', location: { latitude: 0, longitude: 0 } }, 'application/json');
+    const response2: LookupResponses = await fetchTemp.lookupRecord({}, { formattedAddress: '123 Main St', location: { latitude: 0, longitude: 0 } }, 'application/json');
+}
+
+/*
+This doesn't quite get the right results because the generic TTransform can't see the underlying generic on fetchRequest:
+*/
+
+type RequestToResponseTransform = (input: RequestConversion<any, any, any, any>) => Converted<RequestConversion<any, any, any, any>>;
+
+type Converted2<TConversion extends RequestConversion<any, any, any, any>, TTransform extends RequestToResponseTransform> =
+    TConversion extends RequestConversion<infer TParams, infer TBody, infer TResponse, infer TCallType> ?
+        TransformRequest<
+            TParams,
+            TBody,
+            TCallType,
+            TTransform extends (input: RequestConversion<TParams, TBody, TResponse, TCallType>) => TransformRequest<TParams, TBody, TCallType, infer TConvertedResponse>
+                ? TConvertedResponse
+                : unknown
+        > : never;
+
+function applyTransform2<TMethods extends RequestConversions, TTransform extends RequestToResponseTransform>(
+    methods: TMethods,
+    transform: TTransform
+): {
+        [K in keyof TMethods]: Converted2<TMethods[K], TTransform>;
+    } {
+    return Object.keys(methods).reduce((prev, next) => ({ ...prev, [next]: transform(methods[next]) }), {} as {
+        [K in keyof TMethods]: Converted2<TMethods[K], TTransform>;
+    });
+}
+const fetchTemp2 = applyTransform2(allOperations, fetchRequest);
+
+async function testPipe2() {
+    const response: LookupResponses = await fetchLookupRecord({}, { formattedAddress: '123 Main St', location: { latitude: 0, longitude: 0 } }, 'application/json');
+    // const response2: LookupResponses = await fetchTemp2.lookupRecord({}, { formattedAddress: '123 Main St', location: { latitude: 0, longitude: 0 } }, 'application/json');
+}
