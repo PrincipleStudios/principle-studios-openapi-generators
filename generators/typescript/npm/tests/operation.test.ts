53afa11a24122d73852f372c55fe4ccca42749df
Matt DeKrey <mattdekrey@gmail.com>
2021-09-16 07:35:49 -0500

    Consider destructuring responses

--- generators/typescript/npm/tests/operation.test.ts
+++ generators/typescript/npm/tests/operation.test.ts
@@ -1,9 +1,7 @@
-// import { HttpHeaders, HttpQuery, RequestOpts, ResponseArgs, throwIfNullOrUndefined, encodeURI, RequestConversion, StandardResponse, RequestConversions, TransformRequest, TransformRequestWithOptionalBody } from '@principlestudios/openapi-codegen-typescript';
-
 import allOperations from "./no-refs/operations";
-import { conversion as lookupRecord, RequestBodies as LookupRecordRequestBodies, Responses } from "./no-refs/operations/lookupRecord";
+import { conversion as lookupRecord, RequestBodies as LookupRecordRequestBodies, StructuredResponses, Responses } from "./no-refs/operations/lookupRecord";
 import { conversion as getPhoto } from "./no-refs/operations/getPhoto";
-import { RequestConversion, RequestBodies, RequestConversions, TransformRequest } from "./types";
+import { RequestConversion, RequestBodies, RequestConversions, TransformRequest, StandardResponse } from "./types";
 
 lookupRecord.request({}, { formattedAddress: '123 Main St', location: { latitude: 0, longitude: 0 }}, 'application/json');
 // lookupRecord.request({});
@@ -24,3 +22,12 @@ const emptyExtendsRequestBodies: {} extends RequestBodies ? true : false = true;
 
 const extendsRequestConversions: typeof temp extends RequestConversions ? true : false = true;
 const allOperationsExtendsRequestConversions: typeof allOperations extends RequestConversions ? true : false = true;
+
+type DestructureMime<TStatusCode extends number | 'other', T extends { [mimeType: string]: any }> =  { [K in keyof T]: K extends string ? StandardResponse<TStatusCode, K, T[K]> : never }[keyof T];
+type Destructure<T extends { [statusCode: string | number]: { [mimeType: string]: any } }> = { [K in keyof T]: K extends number | 'other' ? DestructureMime<K, T[K]> : never }[keyof T];
+
+type Destructured = Destructure<StructuredResponses>;
+
+// So, it is possible to destructure the response, but... this could end up with complex (therefore slow) type checking. I'd rather not.
+const d: Destructured = { statusCode: 409, mimeType: 'application/json', data: { multiple: {} as any }, response: {} as any };
+
