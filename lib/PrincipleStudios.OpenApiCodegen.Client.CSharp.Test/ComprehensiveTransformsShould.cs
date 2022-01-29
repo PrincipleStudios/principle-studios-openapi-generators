4205c707711d48038d0c81747f45556ea69c9a08
Matt DeKrey <mattdekrey@gmail.com>
2022-01-29 10:30:05 -0600

    Add compiler checking in tests, fix found bugs

--- lib/PrincipleStudios.OpenApiCodegen.Client.CSharp.Test/ComprehensiveTransformsShould.cs
+++ lib/PrincipleStudios.OpenApiCodegen.Client.CSharp.Test/ComprehensiveTransformsShould.cs
@@ -1,8 +1,13 @@
-﻿using PrincipleStudios.OpenApi.CSharp;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CSharp;
+using PrincipleStudios.OpenApi.CSharp;
 using PrincipleStudios.OpenApi.Transformations;
 using Snapshooter.Xunit;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +19,59 @@ namespace PrincipleStudios.OpenApiCodegen.Client.CSharp
 {
     public class ComprehensiveTransformsShould
     {
+        [MemberData(nameof(ValidFileNames))]
+        [Theory]
+        public void Compile(string name)
+        {
+            var document = GetDocument(name);
+            var options = LoadOptions();
+
+            var transformer = document.BuildCSharpPathControllerSourceProvider("", "PS.Controller", options);
+            OpenApiTransformDiagnostic diagnostic = new();
+
+            var entries = transformer.GetSources(diagnostic).ToArray();
+
+            Assert.Empty(diagnostic.Errors);
+
+            var syntaxTrees = entries.Select(e => CSharpSyntaxTree.ParseText(e.SourceText, path: e.Key)).ToArray();
+            
+            string assemblyName = Path.GetRandomFileName();
+            var refPaths = new[] {
+                Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "netstandard.dll"),
+                Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "System.Runtime.dll"),
+                typeof(System.AttributeUsageAttribute).Assembly.Location,
+                typeof(System.Linq.Enumerable).Assembly.Location,
+                typeof(System.ComponentModel.TypeConverter).Assembly.Location,
+                typeof(System.ComponentModel.TypeConverterAttribute).Assembly.Location,
+                typeof(System.ComponentModel.DataAnnotations.RequiredAttribute).Assembly.Location,
+                typeof(System.Runtime.Serialization.EnumMemberAttribute).Assembly.Location,
+
+                typeof(Newtonsoft.Json.JsonConvert).Assembly.Location,
+                Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "System.Net.Http.dll"),
+                Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "System.Net.Primitives.dll"),
+                typeof(Uri).Assembly.Location,
+                typeof(System.Web.HttpUtility).Assembly.Location,
+                typeof(System.Collections.Specialized.NameValueCollection).Assembly.Location,
+            };
+            MetadataReference[] references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: syntaxTrees,
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary) {  });
+
+
+            using var ms = new MemoryStream();
+            var result = compilation.Emit(ms);
+
+            Assert.All(result.Diagnostics, diagnostic =>
+            {
+                Assert.False(diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
+            });
+            Assert.True(result.Success);
+        }
+
         [MemberData(nameof(ValidFileNames))]
         [Theory]
         public void CoverFullFiles(string name)
--- lib/PrincipleStudios.OpenApiCodegen.Server.Mvc.Test/ComprehensiveTransformsShould.cs
+++ lib/PrincipleStudios.OpenApiCodegen.Server.Mvc.Test/ComprehensiveTransformsShould.cs
@@ -1,8 +1,11 @@
-﻿using PrincipleStudios.OpenApi.CSharp;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using PrincipleStudios.OpenApi.CSharp;
 using PrincipleStudios.OpenApi.Transformations;
 using Snapshooter.Xunit;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +17,77 @@ namespace PrincipleStudios.OpenApiCodegen.Server.Mvc
 {
     public class ComprehensiveTransformsShould
     {
+        [MemberData(nameof(ValidFileNames))]
+        //[InlineData("regex-escape.yaml")]
+        [Theory]
+        public void Compile(string name)
+        {
+            var document = GetDocument(name);
+            var options = LoadOptions();
+
+            var transformer = document.BuildCSharpPathControllerSourceProvider("", "PS.Controller", options);
+            OpenApiTransformDiagnostic diagnostic = new();
+
+            var entries = transformer.GetSources(diagnostic).ToArray();
+
+            Assert.Empty(diagnostic.Errors);
+
+            var syntaxTrees = entries.Select(e => CSharpSyntaxTree.ParseText(e.SourceText, path: e.Key)).ToArray();
+
+            string assemblyName = Path.GetRandomFileName();
+            var refPaths = new[] {
+                Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "netstandard.dll"),
+                Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "System.Runtime.dll"),
+                typeof(System.AttributeUsageAttribute).Assembly.Location,
+                typeof(System.Linq.Enumerable).Assembly.Location,
+                typeof(System.ComponentModel.TypeConverter).Assembly.Location,
+                typeof(System.ComponentModel.TypeConverterAttribute).Assembly.Location,
+                typeof(System.ComponentModel.DataAnnotations.RequiredAttribute).Assembly.Location,
+                typeof(System.Runtime.Serialization.EnumMemberAttribute).Assembly.Location,
+                
+                typeof(Microsoft.Extensions.DependencyInjection.IServiceCollection).Assembly.Location,
+                typeof(Microsoft.Extensions.DependencyInjection.IMvcBuilder).Assembly.Location,
+                typeof(Microsoft.Extensions.DependencyInjection.MvcServiceCollectionExtensions).Assembly.Location,
+                typeof(Microsoft.AspNetCore.Mvc.NewtonsoftJson.JsonSerializerSettingsProvider).Assembly.Location,
+
+                typeof(Microsoft.Net.Http.Headers.MediaTypeHeaderValue).Assembly.Location,
+                typeof(Microsoft.Extensions.Primitives.StringSegment).Assembly.Location,
+                typeof(Microsoft.AspNetCore.Mvc.IActionResult).Assembly.Location,
+                typeof(Microsoft.AspNetCore.Mvc.ObjectResult).Assembly.Location,
+                typeof(Microsoft.AspNetCore.Http.HttpContext).Assembly.Location,
+                typeof(Microsoft.AspNetCore.Http.IHeaderDictionary).Assembly.Location,
+                typeof(Newtonsoft.Json.JsonConvert).Assembly.Location,
+                typeof(Microsoft.AspNetCore.Authorization.AuthorizeAttribute).Assembly.Location,
+
+                //typeof(System.Net.HttpStatusCode).Assembly.Location,
+                //typeof(Uri).Assembly.Location,
+                //typeof(ValueType).Assembly.Location,
+                //typeof(Microsoft.AspNetCore.Mvc.Formatters.MediaTypeCollection).Assembly.Location,
+
+                //typeof(System.Web.HttpUtility).Assembly.Location,
+                //typeof(System.Net.Http.HttpClient).Assembly.Location,
+                //typeof(Console).Assembly.Location,
+
+            };
+            MetadataReference[] references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: syntaxTrees,
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary) { });
+
+
+            using var ms = new MemoryStream();
+            var result = compilation.Emit(ms);
+
+            Assert.All(result.Diagnostics, diagnostic =>
+            {
+                Assert.False(diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
+            });
+            Assert.True(result.Success);
+        }
+
         [MemberData(nameof(ValidFileNames))]
         [Theory]
         public void CoverFullFiles(string name)
