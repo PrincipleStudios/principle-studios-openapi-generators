638bb6bc6bdb4467cd1a589db817016c0392d4dd
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-01-18 11:57:07 -0600

    Split TestApp tests to reference each YAML

---
+++ lib/PrincipleStudios.OpenApiCodegen.Server.Mvc.Test/TestApp/EnumYamlShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PrincipleStudios.OpenApiCodegen.Server.Mvc.TestApp;
+
+using static Utilities;
+
+public class EnumYamlShould
+{
+    [Fact]
+    public Task HandleEnumRequestsAndResponses() =>
+        TestSingleRequest<Enum.RockPaperScissorsControllerBase.PlayRockPaperScissorsActionResult>(new(
+            Enum.RockPaperScissorsControllerBase.PlayRockPaperScissorsActionResult.Ok(Enum.PlayRockPaperScissorsResponse.Player1),
+            client => client.PostAsync("/rock-paper-scissors", JsonContent.Create(new { player1 = "rock", player2 = "scissors" }))
+        )
+        {
+            AssertResponseMessage = VerifyResponse(200, "player1"),
+        });
+}
