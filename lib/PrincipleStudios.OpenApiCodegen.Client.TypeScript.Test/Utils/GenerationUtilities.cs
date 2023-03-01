104c7c9d1d0cc779cdf73fe18b3c6ba282e172eb
Matt DeKrey <mattdekrey@gmail.com>
2023-03-01 14:38:43 -0600

    Add TypeScript test for allOf.yaml

--- lib/PrincipleStudios.OpenApiCodegen.Client.TypeScript.Test/Utils/GenerationUtilities.cs
+++ lib/PrincipleStudios.OpenApiCodegen.Client.TypeScript.Test/Utils/GenerationUtilities.cs
@@ -1,18 +1,28 @@
 ﻿using FluentAssertions;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Microsoft.CodeAnalysis;
+using System.IO;
 
 namespace PrincipleStudios.OpenApiCodegen.Client.TypeScript.Utils;
 
 internal static class GenerationUtilities
 {
+    public static async Task<NodeUtility.ProcessResult> ConvertRequest(this CommonDirectoryFixture commonDirectory, string documentName, string operationName, object parameters)
+    {
+        await commonDirectory.PrepareOpenApiDirectory(documentName);
+
+        var result = await commonDirectory.TsNode($@"
+            import {{ RequestOpts }} from '@principlestudios/openapi-codegen-typescript';
+            import {{ conversion }} from './{documentName}/operations/{operationName}';
+            const request: RequestOpts = conversion.request({JsonConvert.SerializeObject(parameters)});
+            console.log(JSON.stringify(request));
+        ");
+        return result;
+    }
+
     public static async Task<NodeUtility.ProcessResult> ConvertRequest(this CommonDirectoryFixture commonDirectory, string documentName, string operationName, object parameters, object body, string contentType = "application/json")
     {
         await commonDirectory.PrepareOpenApiDirectory(documentName);
@@ -26,6 +36,19 @@ internal static class GenerationUtilities
         return result;
     }
 
+    public static async Task<NodeUtility.ProcessResult> CheckModel(this CommonDirectoryFixture commonDirectory, string documentName, string modelName, object body)
+    {
+        await commonDirectory.PrepareOpenApiDirectory(documentName);
+
+        Assert.True(File.Exists(Path.Combine(commonDirectory.DirectoryPath, documentName, "models", $"{modelName}.ts")));
+
+        var result = await commonDirectory.TsNode($@"
+            import {{ {modelName} }} from './{documentName}/models/{modelName}';
+            const model: {modelName} = {JsonConvert.SerializeObject(body)};
+        ");
+        return result;
+    }
+
     // TODO - add extra headers to this
     public static async Task<NodeUtility.ProcessResult> ConvertResponse(this CommonDirectoryFixture commonDirectory, string documentName, string operationName, int statusCode, Optional<object?> body = default, string contentType = "application/json")
     {
@@ -34,9 +57,10 @@ internal static class GenerationUtilities
         var result = await commonDirectory.TsNode($@"
             import {{ ResponseArgs }} from '@principlestudios/openapi-codegen-typescript';
             import {{ conversion, Responses }} from './{documentName}/operations/{operationName}';
+            const responseBody: Responses['data'] = {(body.HasValue ? JsonConvert.SerializeObject(body.Value) : "undefined")};
             const response: ResponseArgs = {{
                 status: {statusCode},
-                response: {(body.HasValue ? JsonConvert.SerializeObject(body.Value) : "undefined")},
+                response: responseBody,
                 getResponseHeader(headerName) {{
                     switch (headerName) {{
                         case 'Content-Type': return {JsonConvert.SerializeObject(contentType)};
@@ -51,15 +75,23 @@ internal static class GenerationUtilities
     }
 
     // TODO - add query string parameters to this
-    public static JToken AssertRequestSuccess(NodeUtility.ProcessResult result, string method, string path, object body, string contentType = "application/json")
+    public static JToken AssertRequestSuccess(NodeUtility.ProcessResult result, string method, string path, Optional<object?> body = default, string contentType = "application/json")
     {
         Assert.Equal(0, result.ExitCode);
 
         var token = JToken.Parse(result.Output);
         Assert.Equal(method, token["method"]?.ToObject<string>());
         Assert.Equal(path, token["path"]?.ToObject<string>());
-        Assert.Equal(contentType, token["headers"]?["Content-Type"]?.ToObject<string>());
-        CompareJson(token["body"], body);
+        if (body.HasValue)
+        {
+            Assert.Equal(contentType, token["headers"]?["Content-Type"]?.ToObject<string>());
+            CompareJson(token["body"], body.Value);
+        }
+        else
+        {
+            Assert.Null(token["headers"]?["Content-Type"]?.ToObject<string>());
+            Assert.Null(token["body"]);
+        }
         return token;
     }
 
