71b1f0b87daa45963ba3d1a077cdcbb9ff8b8790
Matt DeKrey <mattdekrey@gmail.com>
2023-06-16 08:20:39 -0500

    Remove legacy powershell build scripts

--- generators/publish-npm.ps1
+++
@@ -1,3 +0,0 @@
-npm publish .\out\principlestudios-openapi-codegen-typescript-0.2.0.tgz --access public
-#npm publish .\out\principlestudios-openapi-codegen-typescript-axios-0.0.2.tgz --access public
-npm publish .\out\principlestudios-openapi-codegen-typescript-rxjs-0.2.0.tgz --access public
