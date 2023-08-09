6a976bd379e61d1a8d9db89b2e2c9bc28348f63b
Matt DeKrey <mattdekrey@gmail.com>
2023-08-09 17:17:58 -0500

    Use local packages for PR checks

--- examples/dotnetcore-server-interfaces/tooling/build-examples.ps1
+++ examples/dotnetcore-server-interfaces/tooling/build-examples.ps1
@@ -1,15 +1,13 @@
 #!/usr/bin/env pwsh
 
 Param(
-    [Parameter(Mandatory)][String] $VersionSuffix,
-    [Parameter()][String] $githubToken
+    [Parameter(Mandatory)][String] $VersionSuffix
 )
 
 Push-Location "$PSScriptRoot/../../.."
 try {
     docker build . -f examples/dotnetcore-server-interfaces/tooling/Dockerfile `
-        --build-arg VersionSuffix="$VersionSuffix" `
-        --build-arg GitHubToken="$githubToken"
+        --build-arg VersionSuffix="$VersionSuffix"
     if ($Global:LASTEXITCODE -ne 0) {
         throw "Docker build failed with exit code $Global:LASTEXITCODE"
     }
