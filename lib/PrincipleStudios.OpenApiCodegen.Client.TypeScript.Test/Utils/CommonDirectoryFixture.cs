5291de316992fa53d6d9fe17ff413e0dce26c956
Matt DeKrey <mattdekrey@gmail.com>
2024-05-30 11:01:35 -0500

    Improve build for TS integration

--- lib/PrincipleStudios.OpenApiCodegen.Client.TypeScript.Test/Utils/CommonDirectoryFixture.cs
+++ lib/PrincipleStudios.OpenApiCodegen.Client.TypeScript.Test/Utils/CommonDirectoryFixture.cs
@@ -1,25 +1,15 @@
-﻿using PrincipleStudios.OpenApi.Transformations;
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using Xunit;
 
 namespace PrincipleStudios.OpenApiCodegen.Client.TypeScript.Utils;
-
-using static PrincipleStudios.OpenApiCodegen.TestUtils.DocumentHelpers;
-using static PrincipleStudios.OpenApiCodegen.Client.TypeScript.OptionsHelpers;
 
 public class CommonDirectoryFixture : IDisposable
 {
 	public const string CollectionName = "Common Node directory";
 	private readonly CancellationTokenSource cancellation;
-	private readonly ConcurrentDictionary<string, Task> openapiDirectories = new();
 
 	public CommonDirectoryFixture()
 	{
@@ -38,15 +28,9 @@ public class CommonDirectoryFixture : IDisposable
 	public Task Initialized { get; }
 	public CancellationToken CancellationToken => cancellation.Token;
 
-	private async Task Initialize()
+	private Task Initialize()
 	{
-		foreach (var dir in Directory.GetDirectories(DirectoryPath).Where(d => !d.EndsWith("node_modules")))
-		{
-			// clean out the old folders
-			Directory.Delete(dir, recursive: true);
-		}
-
-		await Task.Yield();
+		return Task.CompletedTask;
 	}
 
 	public void SetupProcess(System.Diagnostics.ProcessStartInfo psi)
@@ -68,40 +52,9 @@ public class CommonDirectoryFixture : IDisposable
 		}
 	}
 
-	private async Task GenerateTypeScriptFrom(string documentName)
-	{
-		await Initialized;
-
-		var document = GetDocument(documentName);
-		var options = LoadOptions();
-
-		var transformer = document.BuildTypeScriptOperationSourceProvider("", options);
-		OpenApiTransformDiagnostic diagnostic = new();
-
-		var entries = transformer.GetSources(diagnostic).ToArray();
-		var generatedFolder = Path.Combine(DirectoryPath, documentName);
-		Directory.CreateDirectory(generatedFolder);
-		foreach (var entry in entries)
-		{
-			Directory.CreateDirectory(Path.GetDirectoryName(Path.Combine(generatedFolder, entry.Key))!);
-
-			await File.WriteAllTextAsync(Path.Combine(generatedFolder, entry.Key), contents: entry.SourceText);
-		}
-
-		// Ensure the codegenerated files build
-		var tscResult = await NodeUtility.Tsc(SetupProcess, CancellationToken);
-		if (tscResult.ExitCode != 0)
-		{
-			throw new InvalidOperationException($"tsc failed!\n\n{tscResult.Output}");
-		}
-	}
-
 	public Task PrepareOpenApiDirectory(string documentName)
 	{
-		lock (openapiDirectories)
-		{
-			return openapiDirectories.GetOrAdd(documentName, GenerateTypeScriptFrom);
-		}
+		return Task.CompletedTask;
 	}
 
 	internal async Task<NodeUtility.ProcessResult> TsNode(string source)
